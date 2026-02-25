3693ee740c2fb5d7c3f9f70ebb53aa30f196c9f2
EmreAydo <aydoganemre51@gmail.com>
2026-02-25 22:46:19 +0300

    Implement environment variable logic in flask_option.py

--- flask_option.py
+++ flask_option.py
@@ -4,7 +4,11 @@ import os
 
 def start():
     """returns the right message"""
-    pass  # YOUR CODE HERE
+    env = os.getenv("FLASK_ENV", "empty")
+    if env is None or env == "":
+        return "Starting in empty mode..."
+    else:
+        return f"Starting in {env} mode..."
 
 if __name__ == "__main__":
     print(start())
